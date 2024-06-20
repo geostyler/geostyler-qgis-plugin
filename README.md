3cdd5902fc5a71bb9b7e1306aa27395a595aae35
geographika <sethg@geographika.co.uk>
2024-06-20 15:25:16 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # geostyler-qgis-plugin
-QGIS Plugin for Exporting Layerstyles to different Formats
+QGIS Plugin for Exporting Layerstyles to Different Formats
+
+
+**Experimental**
+
+To test clone the repository and create a symlink pointing to your QGIS plugins directory
+
+Windows Command Prompt:
+
+```
+mklink /D "%APPDATA%\QGIS\QGIS3\profiles\default\python\plugins\geostyler" "C:\GitHub\geostyler-plugin\geostyler"
+```
+
+PowerShell:
+
+```ps
+$AppDataPath = [Environment]::GetFolderPath("ApplicationData")
+$SourcePath = "C:\GitHub\geostyler-plugin\geostyler"
+$LinkPath = Join-Path -Path $AppDataPath -ChildPath "QGIS\QGIS3\profiles\default\python\plugins\geostyler"
+
+# Create symbolic link
+New-Item -ItemType SymbolicLink -Path $LinkPath -Target $SourcePath
+```
+
+Bash (untested):
+
+```
+ln -s /path/to/source /home/username/.local/share/QGIS/QGIS3/profiles/default/python/plugins/geostyler
+```
---
+++ geostyler/client_wrapper.py
@@ -0,0 +1,49 @@
+import subprocess
+import os
+import sys
+
+
+def update_environment_path():
+    # Get the current folder of the script
+    current_folder = os.path.dirname(os.path.abspath(__file__))
+    environment_path = os.getenv("PATH", "")
+
+    # Check if the directory is already in PATH
+    if current_folder not in environment_path:
+        # Add the current folder to the PATH environment variable
+        os.environ["PATH"] = current_folder + os.pathsep + os.environ.get("PATH", "")
+
+
+def run_geostyler(input_file, output_file, output_format):
+    update_environment_path()
+
+    # Define the command and parameters
+    # command = ["geostyler-cli", "--version"]
+
+    command = [
+        "geostyler-cli",
+        input_file,
+        "--source",
+        "qgis",
+        "--target",
+        output_format,
+        "--output",
+        output_file,
+    ]
+
+    # print(" ".join(command))
+
+    # Call the command
+    # return_code = subprocess.call(command)
+    # result = subprocess.run(command, capture_output=True, text=True, encoding="utf")
+
+    # Hide the shell window on Windows
+    if sys.platform == "win32":
+        result = subprocess.run(command, capture_output=True, text=True, encoding="utf", creationflags=subprocess.CREATE_NO_WINDOW)
+    else:
+        result = subprocess.run(command, capture_output=True, text=True, encoding="utf")
+
+#
+#args, returncode, stdout and
+#    stderr
+    return result
